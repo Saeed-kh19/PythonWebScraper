dae9230bd62111272403cd7b74efb7990407a32b
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-01-25 14:01:32 +0330

    deleting filter 'python'

--- project.py
+++ project.py
@@ -15,15 +15,15 @@ results=soup.find(id='ResultsContainer')
 
 job_elements = results.find_all("div",class_='card-content')
 
-python_jobs= results.find_all(
-    'h2', string=lambda text:'python' in text.lower()
-)
+# python_jobs= results.find_all(
+#     'h2', string=lambda text:'python' in text.lower()
+# )
 
-python_job_elements = [
-    h2_element.parent.parent.parent for h2_element in python_jobs 
-]
+# python_job_elements = [
+#     h2_element.parent.parent.parent for h2_element in python_jobs 
+# ]
 
-for job_element in python_job_elements:
+for job_element in job_elements:
     title_element = job_element.find('h2',class_='title')
     company_element = job_element.find('h3',class_='company')
     location_element = job_element.find('p',class_='location')
