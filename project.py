ab8f1dae5d69afe12e80e18aac3a1d719fabfb24
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-01-25 13:52:32 +0330

    adding save in csv

--- project.py
+++ project.py
@@ -1,8 +1,13 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
+counter=0
+titles=[]
+companies=[]
+locations=[]
 
 soup = BeautifulSoup(page.content,'html.parser')
 
@@ -22,7 +27,10 @@ for job_element in python_job_elements:
     title_element = job_element.find('h2',class_='title')
     company_element = job_element.find('h3',class_='company')
     location_element = job_element.find('p',class_='location')
-
+    titles.append(title_element.text.strip())
+    companies.append(company_element.text.strip())
+    locations.append(location_element.text.strip())
+    counter+=1
 
     print()
     print(title_element.text.strip())
@@ -31,42 +39,46 @@ for job_element in python_job_elements:
     print()
     print()
 
-    # links = job_element.find_all("a")
-    # for link in links:
-    #     link_url = link['href']
-    #     print(f'Apply here: {link_url} \n')
-
-    # for job_element in python_job_elements:
-    #     link_url = job_element.find_all("a")[1]["href"]
-    #     print(f"Apply here: {link_url}\n")
-
-
-
-# Import csv module
-import csv
 
 # Open the excel file in write mode
-file = open('project_csv.xlsx', 'w')
-
+file = open('project_csv.csv', 'w')
 # Create a csv writer object
 file = csv.writer(file)
-
 # Write the header row
-file.writerow(['Name', 'Age', 'Enrollment Number'])
-
+file.writerow(['Job_Title', 'Company', 'Location'])
 # Take user input for the number of records
-n = int(input('How many records you want to insert: '))
-
+n = counter
 # Loop through the records and write them to the file
 for i in range(n):
-    name = input(f' {i+1}. Enter name: ')
-    age = input(f' {i+1}. Enter age: ')
-    enroll = input(f' {i+1}. Enter Enrollment number: ')
-    file.writerow([name, age, enroll])
-
+    title = titles[i]
+    company = companies[i]
+    location = locations[i]
+    file.writerow([title, company, location])
 # Close the file
 file.close()
 
-# Print a success message
-print('All records inserted successfully !')
+# # Import csv module
+# import csv
+ 
+# # Open the excel file in write mode
+# file = open('project_csv.xlsx', 'w')
+
+# # Create a csv writer object
+# file = csv.writer(file)
+
+# # Write the header row
+# file.writerow(['Name', 'Age', 'Enrollment Number'])
+
+# # Take user input for the number of records
+# n = counter
+
+# # Loop through the records and write them to the file
+# for i in range(n):
+#     title = input({i+1})
+#     company = input({i+1})
+#     location = input({i+1})
+#     file.writerow([title, company, location])
+
+# # Close the file
+# file.close()
 
